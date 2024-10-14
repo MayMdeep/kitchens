33ebc49c7005805be05050a14e22157091870933
m.deeb <m.deeb@ultrawares.com>
2024-10-14 17:37:56 +0300

    update api - create pivot table

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,33 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+// roles
+use App\Actions\Roles\StoreRoleAction;
+use App\Actions\Roles\DeleteRoleAction;
+use App\Actions\Roles\UpdateRoleAction;
+use App\Actions\Roles\GetRoleAction;
+use App\Actions\Roles\GetRoleListAction;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// permissions
+use App\Actions\Permissions\GetPermissionsByRoleAction;
+use App\Actions\Permissions\AssignPermissionToRoleAction;
+use App\Actions\Permissions\AssignOnePermissionToRoleAction;
+use App\Actions\Permissions\GetPermissionListAction;
+use App\Actions\Permissions\StorePermissionAction;
+
+
+Route::group(['prefix'=>'permissions'], function(){
+    Route::post('assign', AssignPermissionToRoleAction::class);
+    Route::get('', GetPermissionListAction::class);
+    Route::get('{id}',GetPermissionsByRoleAction ::class);
+    Route::put('', StorePermissionAction::class);
+    //AssignOnePermissionToRoleAction
+    Route::post('assignOnePermissionToRole', AssignOnePermissionToRoleAction::class);
+});
+Route::group(['prefix'=>'roles'], function(){
+    Route::get('', GetRoleListAction::class);
+    Route::get('{id}', GetRoleAction::class);
+    Route::put('', StoreRoleAction::class);
+    Route::post('{id}', UpdateRoleAction::class);
+    Route::delete('{id}', DeleteRoleAction::class);
+});
